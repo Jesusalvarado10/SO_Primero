fa197450ceed8710dc8bfb21b090caf55d4c8ad7
Jesus <97445036+Jesusalvarado10@users.noreply.github.com>
2024-09-21 20:58:42 -0400

    first commit

---
+++ src/main/java/com/mycompany/os/OS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.os;
+
+/**
+ *
+ * @author Indatech
+ */
+public class OS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
